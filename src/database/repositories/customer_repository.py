0cdbe9e90ea497329efe2aa0f451be09c672f05d
Jonathan Muniz <jonathan.muniz@lambda3.com.br>
2020-09-15 20:55:45 -0300

    Customer Repository refactor

--- src/database/repositories/customer_repository.py
+++ src/database/repositories/customer_repository.py
@@ -7,23 +7,14 @@ def get(page, per_page):
 def get_by_id(customer_id):
     return Customer.query.filter(Customer.id == customer_id).one()
 
-def create(data):
-    name = data.get('name')
-    email = data.get('email')
-    customer_id = data.get('id')
-
-    customer = Customer(name, email)
-
-    if customer_id:
-        customer.id = customer_id
-
+def create(customer):
     db.session.add(customer)
     db.session.commit()
     db.session.refresh(customer)
 
     return customer
 
-def exists_user_with_email(email):
+def exists_customer_with_email(email):
     return db.session.query(Customer.query.filter(Customer.email == email).exists()).scalar()
 
 
--- src/services/customer_service.py
+++ src/services/customer_service.py
@@ -1,8 +1,9 @@
-from database.repositories.customer_repository import get, create, exists_user_with_email
+from database.repositories.customer_repository import get, create, exists_customer_with_email
 from tools.email_validator import is_valid
 from api.response import Response
 from api.response import ResponseMessages
 from tools.http_status_code import HttpStatusCode
+from database.models import Customer
 
 def get_all(page, per_page):
     return Response(
@@ -11,20 +12,25 @@ def get_all(page, per_page):
         get(page, per_page), 
         None)
 
-def create_customer(user):
-    if is_valid(user['email']):
-        if not exists_user_with_email(user['email']):
+def create_customer(data):
+    if is_valid(data['email']):
+        if not exists_customer_with_email(data['email']):
+            name = data.get('name')
+            email = data.get('email')
+
+            customer = Customer(name, email)
+
             return Response(
                 HttpStatusCode.CREATED.value,
                 ResponseMessages.SUCCESS.value,
-                create(user),
+                create(customer),
                 None)
         else:
             return Response(
             HttpStatusCode.BAD_REQUEST.value,
             ResponseMessages.ERROR.value,
             None,
-            "An user with this e-mail is already registered"
+            "A customer with this e-mail is already registered"
         )
     else:
         return Response(
