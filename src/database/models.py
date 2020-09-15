76a1a86eeda977a6c47df3723fdd0c94a7f134eb
Jonathan Muniz <jonathan.muniz@lambda3.com.br>
2020-09-15 20:16:01 -0300

    Addind wishlist item model

--- src/database/models.py
+++ src/database/models.py
@@ -4,12 +4,22 @@ class Customer(db.Model):
     __tablename__ = 'customers'
 
     id = db.Column(db.Integer, primary_key=True)
-    name = db.Column(db.String(50))
-    email = db.Column(db.String(50), unique=True)
+    name = db.Column(db.String(50), nullable=False)
+    email = db.Column(db.String(50), unique=True, nullable=False)
 
     def __init__(self, name, email):
         self.name = name
         self.email = email
 
-    def __repr__(self):
-        return '<Customer %r>' % self.name
+class WishlistItem(db.Model):
+    __tablename__ = 'wishlist_items'
+
+    id = db.Column(db.Integer, primary_key=True)
+    product_id = db.Column(db.String, unique=True, nullable=False)
+    
+    customer_id = db.Column(db.Integer, db.ForeignKey('customers.id'), nullable=False)
+    customer = db.relationship('Customer', backref=db.backref('posts', lazy='dynamic'))
+
+    def __init__(self, product_id, customer):
+        self.product_id = product_id
+        self.customer = customer
