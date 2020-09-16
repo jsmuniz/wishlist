c36fdc7ad4b7ac3293b6fcb2c5091a773e6ee995
Jonathan Muniz <jonathan.muniz@lambda3.com.br>
2020-09-15 23:13:06 -0300

    Fixing table name

--- src/database/models.py
+++ src/database/models.py
@@ -1,5 +1,6 @@
 from database import db
 
+
 class Customer(db.Model):
     __tablename__ = 'customers'
 
@@ -11,25 +12,31 @@ class Customer(db.Model):
         self.name = name
         self.email = email
 
+
 class Wishlist(db.Model):
     __tablename__ = 'wishlists'
 
     id = db.Column(db.Integer, primary_key=True)
-    
-    customer_id = db.Column(db.Integer, db.ForeignKey('customers.id'), unique=True, nullable=False)
-    customer = db.relationship('Customer', backref=db.backref('wishlists', lazy='dynamic'))
+
+    customer_id = db.Column(db.Integer, db.ForeignKey(
+        'customers.id'), unique=True, nullable=False)
+    customer = db.relationship(
+        'Customer', backref=db.backref('wishlists', lazy='dynamic'))
 
     def __init__(self, customer):
         self.customer = customer
 
+
 class WishlistItem(db.Model):
-    __tablename__ = 'wishlist_itens'
+    __tablename__ = 'wishlist_items'
 
     id = db.Column(db.Integer, primary_key=True)
     product_id = db.Column(db.String(50), unique=True, nullable=False)
 
-    wishlist_id = db.Column(db.Integer, db.ForeignKey('wishlists.id'), unique=False, nullable=False)
-    wishlist = db.relationship('Wishlist', backref=db.backref('wishlist_itens', lazy='dynamic'))
+    wishlist_id = db.Column(db.Integer, db.ForeignKey(
+        'wishlists.id'), unique=False, nullable=False)
+    wishlist = db.relationship('Wishlist', backref=db.backref(
+        'wishlist_itens', lazy='dynamic'))
 
     def __init__(self, product_id, wishlist):
         self.product_id = product_id
